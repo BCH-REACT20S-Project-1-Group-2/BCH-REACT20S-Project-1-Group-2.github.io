350a062cf01e7f0cefd122a1d770394d76efb5d3
Rishi Chalise <chaliserishi@gmail.com>
2020-10-27 10:17:36 +0200

    Feature/react codebase bootstrap, Feature/test automation (#6)
    * feature/react_codebase_bootstrap: bootstraped with create react app
    
    * Merging 'feature/test_automation' into feature/react_codebase_bootstrap
    
    * feature/test_automation: added dependencies for test automation framework (Nightwatch.js)
    
    Co-authored-by: Risman <Risman@Rishis-MBP.lan>
    Co-authored-by: Vesa Suikkola <vesa.suikkola@gmail.com>

---
+++ frontend/nightwatch.conf.js
@@ -0,0 +1,21 @@
+module.exports = {
+  src_folders: ["./test"],
+  output_folder: "./test/reports",
+
+  webdriver: {
+    start_process: true,
+    server_path: require("chromedriver").path,
+    port: 9515,
+  },
+
+  test_settings: {
+    default: {
+      desiredCapabilities: {
+        browserName: "chrome",
+        chromeOptions: {
+          args: ["--headless"],
+        },
+      },
+    },
+  },
+};
