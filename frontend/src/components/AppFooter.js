8dc7c44827b479605d5f883db10140d496edf470
Risman <Risman@Rishis-MBP.lan>
2020-10-29 23:31:55 +0200

    feature/AppFooter:add icons and styles on footer

--- frontend/src/components/AppFooter.js
+++ frontend/src/components/AppFooter.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/AppFooter.css";
+import { FaTachometerAlt, FaChartBar, FaSignOutAlt } from 'react-icons/fa';
 
 function AppFooter() {
   return (
     <div className="AppFooter">
       <div>
-        <Link to="/landing">Icon 1: dashboard/landing</Link>
+      <Link to="/landing"><FaTachometerAlt /></Link>
       </div>
       <div>
-        <Link to="/landing">Icon 2: analysis (to be added in MVP+)</Link>
+      <Link to="/landing"><FaChartBar /></Link> 
       </div>
       <div>
-        <Link to="/">Icon 3: logout</Link>
+      <Link to="/"><FaSignOutAlt /></Link>
       </div>
     </div>
   );
