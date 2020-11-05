422ab37f751a121d43c59e5ce6d8e5cb950dc6a6
Vesa Suikkola <vesa.suikkola@gmail.com>
2020-11-05 11:48:51 +0200

    bugfix/frontend-skeleton/component-structure: Footer component refactored

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,28 +1,51 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import { Switch, Route } from 'react-router-dom';
 import AppFooter from './AppFooter';
-import CustomModal from './CustomModal';
-import '../styles/Footer.css';
+import CustomModal from "./CustomModal";
 
 function Footer() {
   return (
-    <footer>
-      <Switch>
-        <Route path="/landing">
-          <AppFooter />
-        </Route>
-        <Route path="/expense">
-          <AppFooter />
-        </Route>
-        <Route path="/">
-          <CustomModal title="About" body="About lorem ipsum" />
-          <CustomModal
-            title="Privacy Policy"
-            body="Privacy policy lorem ipsum"
-          />
-        </Route>
-      </Switch>
-      <div id="copyright">Copyright &copy; BudgetCrtl 2020</div>
+    <footer
+      className="p-2"
+      style={{
+        height: "78px",
+      }}
+    >
+      <Container>
+        <Switch>
+          <Route path="/landing">
+            <AppFooter />
+          </Route>
+          <Route path="/expense">
+            <AppFooter />
+          </Route>
+          <Route path="/"> 
+            <Row>
+              <Col>
+                <CustomModal 
+                  title="About"
+                  body="About lorem ipsum"
+                />
+              </Col>
+              <Col className="d-flex justify-content-end">
+                <CustomModal 
+                  title="Privacy Policy"
+                  body="Privacy policy lorem ipsum"
+                />
+              </Col>
+            </Row>
+
+          </Route>
+        </Switch>
+        <Row>
+          <Col>
+            <h6 className="d-flex justify-content-center mt-3">
+              Copyright &copy; BudgetCrtl 2020
+            </h6>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 }
