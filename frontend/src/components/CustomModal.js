3323525ef9c64692181fb1e19106d7ac3305b273
Vesa Suikkola <vesa.suikkola@gmail.com>
2020-11-05 09:47:17 +0200

    bugfix/frontend-skeleton/component-structure: CustomModal component refactored

--- frontend/src/components/CustomModal.js
+++ frontend/src/components/CustomModal.js
@@ -9,15 +9,24 @@ function CustomModal(props) {
   const handleShow = () => setShow(true);
 
   return (
-    <div className="CustomModal" id={props.title.split(' ')[0]}>
+    <div 
+      className="CustomModal"
+      id={props.title.split(" ")[0]}
+      style={{
+        cursor: "pointer"
+      }}>
       <div onClick={handleShow}>{props.title}</div>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header className="align-self-center">
-          <Modal.Title>{props.title}</Modal.Title>
+      <Modal show={show} onHide={handleClose} className="mt-5 pt-5">
+        <Modal.Header className="align-self-center border-0" >
+          <Modal.Title>
+            <h3>{props.title}</h3>
+          </Modal.Title>
         </Modal.Header>
-        <Modal.Body>{props.body}</Modal.Body>
-        <Modal.Footer>
+        <Modal.Body>
+          {props.body}
+        </Modal.Body>
+        <Modal.Footer className="align-self-center border-0">
           <Button onClick={handleClose}>Close</Button>
         </Modal.Footer>
       </Modal>
