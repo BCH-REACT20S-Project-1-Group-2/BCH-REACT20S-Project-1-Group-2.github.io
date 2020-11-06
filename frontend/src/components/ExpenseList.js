2fa0a465214179c888c62d1eb614f20c76148d85
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2020-11-06 08:38:27 +0200

    Expense list updated: Expense list updated with state and map

--- frontend/src/components/ExpenseList.js
+++ frontend/src/components/ExpenseList.js
@@ -4,17 +4,36 @@ import { FaSave } from "react-icons/fa";
 import "../styles/ExpenseList.css";
 
 class ExpenseList extends React.Component {
+  state = {
+    expenses: [
+      { categoryid: 1, categoryName: "Rent & Mortgage" },
+      { categoryid: 2, categoryName: "Groceries" },
+      { categoryid: 3, categoryName: "Loan payments" },
+      { categoryid: 4, categoryName: "Insurances" },
+      { categoryid: 5, categoryName: "Fuel/ Transportation costs" },
+      { categoryid: 6, categoryName: "Clothing & Shoes" },
+      { categoryid: 7, categoryName: "Entertainment & Hobbies" },
+      {
+        categoryid: 8,
+        categoryName: "Utility bills (cable, cell, electricity, water, etc",
+      },
+      { categoryid: 9, categoryName: "Health expenses" },
+      { categoryid: 10, categoryName: "Services (daycare, hair dresser)" },
+      { categoryid: 11, categoryName: "Miscellenous" },
+    ],
+  };
   render() {
+    const expenseList = this.state.expenses.map((item) => {
+      return <option>{item.categoryName}</option>;
+    });
+
     return (
       <>
         <Form className="expenseform">
           <Form.Group controlId="expenseList" className="expenseform">
             <Form.Control as="select" custom defaultValue="Add category">
               <option disabled>Add category</option>
-              <option>Rent</option>
-              <option>Mortgage(s)</option>
-              <option>Utility Bills</option>
-              <option>Insurance</option>
+              {expenseList}
             </Form.Control>
             <Form.Control type="number" placeholder="e.g. 100 €" step=".01" />
             <FaSave className="SaveIcon" />
