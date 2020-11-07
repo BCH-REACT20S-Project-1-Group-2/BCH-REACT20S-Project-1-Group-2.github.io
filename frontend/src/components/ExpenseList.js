b406cb6cf511b73eb57494f0a6c318d91d0fbe58
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2020-11-07 11:24:26 +0200

    Add expenses working with form validation checks without LS

--- frontend/src/components/ExpenseList.js
+++ frontend/src/components/ExpenseList.js
@@ -4,19 +4,58 @@ import { FaSave } from "react-icons/fa";
 import "../styles/ExpenseList.css";
 
 class ExpenseList extends React.Component {
+  state = {
+    userexpense: { categoryName: "", amount: "" },
+  };
+
+  onChange = (e) => {
+    // this changes the state of userexpenses amount.
+    this.setState((prevState) => {
+      let userexpense = Object.assign({}, prevState.userexpense);
+      userexpense[e.target.name] = e.target.value;
+      return { userexpense };
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (!e.target.checkValidity()) {
+      return;
+    } else {
+      //this function is defined at expensepage and its updates total expenses
+      this.props.updateTotal(this.state.userexpense.amount);
+
+      //Clears the fields after taking input
+      e.target.reset();
+    }
+  };
+
   render() {
     return (
       <>
-        <Form className="expenseform">
+        <Form noValidate onSubmit={this.onSubmit} className="expenseform">
           <Form.Group controlId="expenseList" className="expenseform">
-            <Form.Control as="select" custom defaultValue="Add category">
+            <Form.Control
+              as="select"
+              custom
+              defaultValue="Add category"
+              required
+            >
               <option disabled>Add category</option>
               <option>Rent</option>
               <option>Mortgage(s)</option>
               <option>Utility Bills</option>
               <option>Insurance</option>
             </Form.Control>
-            <Form.Control type="number" placeholder="e.g. 100 €" step=".01" />
+            <Form.Control
+              type="number"
+              placeholder="e.g. 100 €"
+              step=".01"
+              required
+              name="amount"
+              min="0.01"
+              onChange={this.onChange.bind(this)}
+            />
             <label htmlFor="savebutton">
               <FaSave className="SaveIcon" />
             </label>
--- frontend/src/components/ExpensePage.js
+++ frontend/src/components/ExpensePage.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./Header";
 import ExpensePresentation from "./ExpensePresentation";
 import ExpenseList from "./ExpenseList";
 import Footer from "./Footer";
+
 import "../styles/ExpensePage.css";
 
-function ExpensePage() {
-  return (
-    <div className="ExpensePage">
-      <Header />
-      <main>
-        <ExpensePresentation />
-        <ExpenseList />
-      </main>
-      <Footer />
-    </div>
-  );
+export class ExpensePage extends Component {
+  state = {
+    total: 0,
+  };
+
+  updateTotal = (amount) => {
+    this.setState({ total: this.state.total + parseFloat(amount) });
+  };
+
+  render() {
+    return (
+      <div className="ExpensePage">
+        <Header />
+        <main>
+          <ExpensePresentation total={this.state.total} />
+          <ExpenseList updateTotal={this.updateTotal} />
+        </main>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default ExpensePage;
