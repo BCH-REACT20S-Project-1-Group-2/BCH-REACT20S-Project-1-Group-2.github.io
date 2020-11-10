2a20d623dd1121d46a9b173cb540dc2c5c338566
Risman <Risman@Rishis-MBP.lan>
2020-11-10 13:09:29 +0200

    feature/ShowUserName: add local storage to display username on header after user logged in

--- frontend/src/components/AppHeader.js
+++ frontend/src/components/AppHeader.js
@@ -2,22 +2,28 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "../styles/AppHeader.css";
 
-function AppHeader() {
-  return (
-    <Container>
+
+export default class AppHeader extends React.Component {
+
+  state = {
+    username: localStorage.getItem("user")
+  };
+
+  render() {
+    return (
+      <Container>
       <Row className="align-items-center mb-2">
         <Col>
           <h1 className="m-0">Logo</h1>
         </Col>
         <Col xs={6}>
-          <h3 className="m-0">user name</h3>
+          <h3 className="m-0">{this.state.username}</h3>
         </Col>
         <Col>
           <div className="m-0 userIcon"></div>
         </Col>
       </Row>
     </Container>
-  );
+    );
+  }
 }
-
-export default AppHeader;
--- frontend/src/components/WelcomePage.js
+++ frontend/src/components/WelcomePage.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import Header from "./Header";
 import Footer from "./Footer";
 import "../styles/WelcomePage.css";
 
-function WelcomePage() {
-  return (
-    <div className="WelcomePage">
+
+class WelcomePage extends Component {
+
+  state = {
+    username: "",
+  }
+  onChangeName = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleLogin = (e) => {
+    localStorage.setItem("user",(this.state.username));
+  }
+
+  onSubmit = (e) => {
+    this.setState({
+      username: "",
+    })
+  };
+
+
+  render() {
+    return (
+      <div className="WelcomePage">
       <Header />
       <main>
         <h2 id="welcomeText">Welcome Text</h2>
-        <Form>
+        <Form onSubmit={this.onSubmit}>
           <Form.Group controlId="userName">
             <Form.Control
               type="text"
               placeholder="your name"
               className="text-center"
+              onChange={this.onChangeName} name="username"
             />
           </Form.Group>
           <Link to="/landing">
-            <Button id="welcomeCTA" className="w-100 text-white">
+            <Button onClick={this.handleLogin} id="welcomeCTA" className="w-100 text-white">
               Let's begin
             </Button>
           </Link>
@@ -28,7 +52,8 @@ function WelcomePage() {
       </main>
       <Footer />
     </div>
-  );
+    );
+  }
 }
 
 export default WelcomePage;
