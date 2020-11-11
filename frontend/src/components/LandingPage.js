f843706ade99ac94a83632b2a102540d8a9bb9b4
Vesa Suikkola <vesa.suikkola@gmail.com>
2020-11-11 13:41:45 +0200

    bugfix/frontend-skeleton/component-structure: preparing to merge with dev

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { Form, Button, Accordion, Card } from "react-bootstrap";
+import {
+  InputGroup,
+  FormControl,
+  Button,
+  Accordion,
+  Card,
+  Form,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
-import Header from "./Header";
 import BalancePresentation from "./BalancePresentation";
-import Footer from "./Footer";
-import "../styles/LandingPage.css";
 
-function LandingPage() {
+function LandingMain() {
   const {
     updateSalaryLabel,
     salaryInputLabel,
@@ -14,60 +18,72 @@ function LandingPage() {
     savingInputLabel,
     expensePageLinkLabel,
   } = require("../phrases/LandingPage.json");
-  const {saveButtonLabel} = require('../phrases/App.json')
+  const { saveButtonLabel } = require("../phrases/App.json");
 
   return (
-    <div className="LandingPage">
-      <Header />
-      <main>
-        <BalancePresentation />
-        <Accordion>
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="0">
-              <Button className="w-100 text-white">{updateSalaryLabel}</Button>
+    <main>
+      <BalancePresentation />
+      <Accordion className="mt-3">
+        <Card className="mb-3">
+          <Accordion.Toggle as={Button} eventKey="0" className="w-100">
+            {updateSalaryLabel}
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>
+              <Form>
+                <InputGroup>
+                  <FormControl
+                    placeholder={salaryInputLabel}
+                    className="text-center"
+                    type="number"
+                    step=".01"
+                  />
+                  <InputGroup.Append>
+                    <InputGroup.Text>€</InputGroup.Text>
+                    <InputGroup.Text as={Button}>
+                      {saveButtonLabel}
+                    </InputGroup.Text>
+                  </InputGroup.Append>
+                </InputGroup>
+              </Form>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card className="mb-3">
+          <Accordion.Toggle as={Button} eventKey="1" className="w-100">
+            {updateSavingAmoungLabel}
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>
+              <Form>
+                <InputGroup>
+                  <FormControl
+                    placeholder={savingInputLabel}
+                    className="text-center"
+                    type="number"
+                    step=".01"
+                  />
+                  <InputGroup.Append>
+                    <InputGroup.Text>€</InputGroup.Text>
+                    <InputGroup.Text as={Button}>
+                      {saveButtonLabel}
+                    </InputGroup.Text>
+                  </InputGroup.Append>
+                </InputGroup>
+              </Form>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card className="mb-3">
+          <Link to="/expense">
+            <Accordion.Toggle as={Button} className="w-100">
+              {expensePageLinkLabel}
             </Accordion.Toggle>
-            <Accordion.Collapse eventKey="0">
-              <Card.Body>
-                <Form className="form-wrapper">
-                  <Form.Group controlId="formSalary">
-                    <Form.Label>{salaryInputLabel}</Form.Label>
-                    <Form.Control type="number" />
-                    <p>€</p>
-                  </Form.Group>
-                  <Button variant="primary">{saveButtonLabel}</Button>
-                </Form>
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card>
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="1">
-              <Button className="w-100 text-white">{updateSavingAmoungLabel}</Button>
-            </Accordion.Toggle>
-            <Accordion.Collapse eventKey="1">
-              <Card.Body>
-                <Form className="form-wrapper">
-                  <Form.Group controlId="formSavings">
-                    <Form.Label>{savingInputLabel}</Form.Label>
-                    <Form.Control type="number" />
-                    <p>€</p>
-                  </Form.Group>
-                  <Button variant="primary">{saveButtonLabel}</Button>
-                </Form>
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card>
-          <Card>
-            <Link to="/expense">
-              <Accordion.Toggle as={Card.Header}>
-                <Button className="w-100 text-white">{expensePageLinkLabel}</Button>
-              </Accordion.Toggle>
-            </Link>
-          </Card>
-        </Accordion>
-      </main>
-      <Footer />
-    </div>
+          </Link>
+        </Card>
+      </Accordion>
+    </main>
   );
 }
 
-export default LandingPage;
+export default LandingMain;
