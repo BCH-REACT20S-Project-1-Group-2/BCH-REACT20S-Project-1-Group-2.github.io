134a948e440dfb26e4fbe1fa492960dd147680f0
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2020-11-12 16:50:38 +0200

    Conflicts resolved with dev

--- frontend/src/components/AppFooter.js
+++ frontend/src/components/AppFooter.js
@@ -1,21 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/AppFooter.css";
-import { FaTachometerAlt, FaChartBar, FaSignOutAlt } from 'react-icons/fa';
+import { FaTachometerAlt, FaChartBar, FaSignOutAlt } from "react-icons/fa";
+import { Container, Row, Col } from "react-bootstrap";
 
 function AppFooter() {
   return (
-    <div className="AppFooter">
-      <div>
-      <Link to="/landing"><FaTachometerAlt /></Link>
-      </div>
-      <div>
-      <Link to="/landing"><FaChartBar /></Link> 
-      </div>
-      <div>
-      <Link to="/"><FaSignOutAlt /></Link>
-      </div>
-    </div>
+    <Container className="p-2 text-center">
+      <Row>
+        <Col>
+          <Link to="/landing">
+            <FaTachometerAlt />
+          </Link>
+        </Col>
+        <Col className="border-left border-right">
+          <Link to="/landing">
+            <FaChartBar />
+          </Link>
+        </Col>
+        <Col>
+          <Link to="/">
+            <FaSignOutAlt />
+          </Link>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
